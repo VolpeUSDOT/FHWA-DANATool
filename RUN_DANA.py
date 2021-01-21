12149a42f59ea3ae44ca7359b327e71eb54e4354
AaronHastingsVolpe <Aaron.Hastings@dot.gov>
2021-01-21 12:54:12 -0500

    Updated RUN_DANA

--- RUN_DANA.py
+++ RUN_DANA.py
@@ -10,42 +10,61 @@ from lib import NTD_02_MOVES
 from lib import NTD_03_SPEED
 from lib import NTD_04_NOISE
 
-state  = 'MA'
+# Basic Input Parameters
 step = 1
-county = 'Middlesex'
-SELECT_TMC = ['129+04374', '129P09003']
 year = 2017
+state  = 'MA'
+county = 'Middlesex'
+
+SELECT_TMC = ['129+04374', '129P09003']
+SELECT_STATE = state 
 
 
-SELECT_STATE = state
+# Setup Path Prefixes Based on Users Computer
+computerName = os.environ['ComputerName']
+if computerName == 'VOLSLBOS-43873':
+    pathPrefix1 = 'D:/Repos/FHWA-DANATool/Default Input Files'
+    pathPrefix2 = 'D:/Repos/FHWA-DANATool/User Input Files/Example_MiddlesexCounty_Massachusetts/2018 NPMRDS Data'
+    pathPrefix3 = 'D:/Repos/FHWA-DANATool/NPMRDS_Intermediate_Output'
+elif computerName == 'OFFICEDESKTOP':
+    pathPrefix1 = 'G:/Repos/FHWA-DANATool/Default Input Files'
+    pathPrefix2 = 'G:/Repos/FHWA-DANATool/User Input Files/Example_MiddlesexCounty_Massachusetts/2018 NPMRDS Data'
+    pathPrefix3 = 'G:/Repos/FHWA-DANATool/NPMRDS_Intermediate_Output'
+elif computerName == 'TBD'
+    pathPrefix1 = 'TBD'
+    pathPrefix2 = 'TBD'
+    pathPrefix3 = 'TBD'
 
-PATH_TMAS_STATION = 'C:/DANAToolV3/Default Input Data/TMAS Data/TMAS {}/tmas_station_{}.csv'.format(year, year)
-PATH_TMAS_CLASS_CLEAN = 'C:/DANAToolV3/Default Input Data/TMAS Data/TMAS {}/tmas_class_clean_{}.csv'.format(year, year)
-PATH_FIPS = 'C:/DANAToolV3/Default Input Data/FIPS_County_Codes.csv'
-PATH_NEI = 'C:/DANAToolV3/Default Input Data/NEI_Representative_Counties.csv'
 
-PATH_npmrds_raw_all = 'C:/DANAToolV3/User Input Data/Example_MiddlesexCounty_Massachusetts/2018 NPMRDS Data/{}_{}_2018_ALL.csv'.format(state, county, state, county.upper())
-PATH_npmrds_raw_pass = 'C:/DANAToolV3/User Input Data/Example_MiddlesexCounty_Massachusetts/2018 NPMRDS Data/{}_{}_2018_PASSENGER.csv'.format(state, county, state, county.upper())
-PATH_npmrds_raw_truck = 'C:/DANAToolV3/User Input Data/Example_MiddlesexCounty_Massachusetts/2018 NPMRDS Data/{}_{}_2018_TRUCKS.csv'.format(state, county, state, county.upper())
-PATH_tmc_identification = 'C:/DANAToolV3/User Input Data/Example_MiddlesexCounty_Massachusetts/2018 NPMRDS Data/TMC_Identification.csv'.format(state, county)
-PATH_tmc_shp = 'C:/DANAToolV3/Default Input Data/National TMC Shapefile/NationalMerge.shp'
-PATH_emission = 'C:/DANAToolV3/Default Input Data/NEI_National_Emissions_Rates_Basis.csv'
+# Set File Paths for Calling DANA Scripts
+PATH_TMAS_STATION = pathPrefix1 + '/TMAS Data/TMAS {}/tmas_station_{}.csv'.format(year, year)
+PATH_TMAS_CLASS_CLEAN = pathPrefix1 + '/TMAS Data/TMAS {}/tmas_class_clean_{}.csv'.format(year, year)
+PATH_FIPS = pathPrefix1 + '/FIPS_County_Codes.csv'
+PATH_NEI = pathPrefix1 + '/NEI_Representative_Counties.csv'
 
-PATH_HPMS  = 'C:/DANAToolV2/Data Input/HPMS/{}_HPMS_2017.csv'.format(state)
-PATH_VM2 = 'C:/DANAToolV2/Data Input/HPMS/VM2_2017.csv' 
-PATH_COUNTY_MILEAGE = 'C:/DANAToolV2/Data Input/HPMS/National_2017_11_AdHocSQL.csv'
+PATH_npmrds_raw_all = pathPrefix2 + '/{}_{}_2018_ALL.csv'.format(state.upper(), county.upper())
+PATH_npmrds_raw_pass = pathPrefix2 + '/{}_{}_2018_PASSENGER.csv'.format(state.upper(), county.upper())
+PATH_npmrds_raw_truck = pathPrefix2 + '/{}_{}_2018_TRUCKS.csv'.format(state.upper(), county.upper())
+PATH_tmc_identification = pathPrefix2 + '/TMC_Identification.csv'
 
-PATH_NPMRDS = 'C:/DANAToolV2/Step2_Outputs/{}/{}/{}_Composite_Emissions.parquet'.format(state, county, state)
+PATH_tmc_shp = pathPrefix1 + '/National TMC Shapefile/NationalMerge.shp'
+PATH_emission = pathPrefix1 + '/NEI_National_Emissions_Rates_Basis.csv'
+
+PATH_HPMS  = pathPrefix2 + '/HPMS/{}_HPMS_{}.csv'.format(state.upper(), year) # Need to confirm - ALH
+PATH_VM2 = pathPrefix2 + '/HPMS/VM2_{}.csv'.format(year) # Need to confirm - ALH
+PATH_COUNTY_MILEAGE = pathPrefix1 + '/HPMS/National_{}_11_AdHocSQL.csv'.format(year) # Need to confirm - ALH
+
+PATH_NPMRDS = pathPrefix3 + '/{}/{}/{}_Composite_Emissions.parquet'.format(state, county, state) # Need to confirm - ALH
 
 
 
 if step == 1:
     NTD_01_NPMRDS.NPMRDS(SELECT_STATE, PATH_tmc_identification, PATH_tmc_shp, PATH_npmrds_raw_all, PATH_npmrds_raw_pass, PATH_npmrds_raw_truck, PATH_emission, PATH_TMAS_STATION, PATH_TMAS_CLASS_CLEAN, PATH_FIPS, PATH_NEI)
-elif step==2:
+elif step == 2:
     NTD_02_MOVES_MOVES.MOVES(SELECT_STATE, PATH_TMAS_CLASS_CLEAN, PATH_HPMS, PATH_VM2, PATH_COUNTY_MILEAGE)
-elif step==3:
+elif step == 3:
     NTD_03_SPEED.SPEED(SELECT_STATE, PATH_NPMRDS)  
-elif step==4:
+elif step == 4:
     NTD_04_NOISE.NOISE(SELECT_STATE, SELECT_TMC, PATH_NPMRDS)
 
     
